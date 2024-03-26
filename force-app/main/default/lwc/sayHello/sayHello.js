df444f07d0f0f0d550f6bd0c229ce27d988d6225
Sumit Mehare <sumitmehare@gmail.com>
2024-03-26 11:48:56 +0530

    API Decorator

--- force-app/main/default/lwc/sayHello/sayHello.js
+++ force-app/main/default/lwc/sayHello/sayHello.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class SayHello extends LightningElement {}
+export default class SayHello extends LightningElement {
+    parent;
+    handlemessage(event){
+        this.parent = this.template.querySelector('c-say-hello-child').message;
+        return this.parent
+    }
+    chnagemessage(event){
+        this.parent = this.template.querySelector('c-say-hello-child').changeMessage();
+    }
+}
---
+++ force-app/main/default/lwc/sayHelloChild/sayHelloChild.js
@@ -0,0 +1,10 @@
+import { LightningElement, api } from 'lwc';
+
+export default class SayHelloChild extends LightningElement {
+    @api message = 'Hey I am from child';
+    @api
+    changeMessage(){
+        this. message = 'Hey I am changed';
+        return this.message;
+    }
+}
